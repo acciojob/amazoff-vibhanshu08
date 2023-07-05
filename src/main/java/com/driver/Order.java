2e5baf29c50454f2193c64dd1a8e5c1d03f0bfeb
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-07-05 08:37:28 +0530

    Update Order.java

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,6 +9,10 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+        String arr[] = deliveryTime.split(":");
+        int newtime = Integer.parseInt(arr[0])*60 + Integer.parseInt(arr[1]);
+        this.deliveryTime = newtime;
     }
 
     public String getId() {
@@ -16,4 +20,12 @@ public class Order {
     }
 
     public int getDeliveryTime() {return deliveryTime;}
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id='" + id + '\'' +
+                ", deliveryTime=" + deliveryTime +
+                '}';
+    }
 }
